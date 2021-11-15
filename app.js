401e2327dac8516f755ba3fe7f1e130897659e01
Mariamopataraia <mariamopataraia@gmail.com>
2021-11-15 16:03:43 +0400

    updated  html

---
+++ app.js
@@ -0,0 +1,1 @@
+[{"id":3,"first_name":"giorgi","last_name":"keshikashvili","zip":"0163","mobile":"568691784","pn":"01001028111","gender":"male","email":"keshikashviligio@gmail.com","status":"active","created_at":null,"updated_at":null}]
